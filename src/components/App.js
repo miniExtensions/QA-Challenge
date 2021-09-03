c751bc0082905726cb0a0f390a059838c3dd3b7e
Abdulrahman AlZanki <bojamal7@gmail.com>
2021-09-03 12:44:20 -0700

    initial

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
+
+
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer);
+
+const Container = styled.div`
+  background-color: #f1f1f1;
+  margin: 0;
+  min-height: 100vh;
+`;
+
+const Paper = styled.div`
+  width: 480px !important;
+  background-color: white;
+`;
+
+// Main app structure
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+
+      <Container className="row flex-center flex-middle">
+        <Paper className="border border-primary padding-large margin-large no-responsive">
+          <h3 className="margin-small">PaperTodo</h3>
+          <AddTodo />
+          <TodoList />
+        </Paper>
+      </Container>      
+    </Provider>
+    );
+  }
+}
+
+export default App;
