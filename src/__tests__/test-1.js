745cfc8a20fc75e8847e8b343df8b741ed10f305
Abdulrahman AlZanki <bojamal7@gmail.com>
2021-09-03 13:12:53 -0700

    more

--- src/__tests__/test-1.js
+++ src/__tests__/test-1.js
@@ -2,12 +2,15 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import App from '../components/App'
 
-test('Test that you can add a new todo list item, then click it to mark it as completed, and then change the filter to "active"', () => {
+test('Challenge', () => {
   render(<App />)
 
   expect(screen.getByText('PaperTodo')).toBeVisible()
 
-  
-  
   // Add your test here
+
+// 1) Add a new todo list item
+// 2) Check that the item that you added is now visible on the screen
+// 3) Click the clear button
+// 4) Check that "Chill scenes." is visible, and that the item you added is not visible anymore
 })
